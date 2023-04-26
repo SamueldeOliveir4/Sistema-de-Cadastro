0044f3b46b60d3219fe611f2f1dbd0ed772d2cdf
Samuel Augusto Guilherme de Oliveira <samuelaugustoguilherme@gmail.com>
2023-04-26 09:57:52 -0300

    Criando a Pagina de Contato e Observacoes git

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos");
 console.log("A aplicacao esta se conectando com o banco de dados ")
+/*
+    Comandos git 
+
+    git clone = para clonar o arquivo
+    git pull = Traz altercoes feitas 
+    git commit . -m "mensagem " = realizar o commit 
+    git push  origin = enviar alteracoes para o repositorio 
+    git log --online = mostra todos os logs das modificacoes 
+    git restore --source "hash" = retorna o codigo para a versao escolhida
+    git log -p = ver alteracoes do commit
+
+*/
